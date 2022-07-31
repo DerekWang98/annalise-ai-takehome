bdfc8c337cf6b5e03d8a66d5ad2346ce9bb571f4
DerekWang98 <derekwhw@gmail.com>
2022-07-31 18:02:03 +1000

    Update config with the correct entities

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,10 +1,10 @@
 import { Options } from '@mikro-orm/core';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
-import { Author, Book, BookTag, Publisher, BaseEntity } from './entities';
+import { Image, Tag, BaseEntity } from './entities';
 
 const options: Options = {
   type: 'postgresql',
-  entities: [Author, Book, BookTag, Publisher, BaseEntity],
+  entities: [Image, Tag, BaseEntity],
   dbName: 'mikro-orm-express-ts',
   highlighter: new SqlHighlighter(),
   debug: true,
