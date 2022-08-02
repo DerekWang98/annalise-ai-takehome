17e9fe73e061cf08db907693da86fd9f69561ba6
DerekWang98 <derekwhw@gmail.com>
2022-08-02 22:26:06 +1000

    Add tests for image

---
+++ tests/image.test.ts
@@ -0,0 +1,58 @@
+import { assert } from "chai";
+import { request } from "supertest";
+import { app } from "../src/server";
+const port = process.env.PORT || 3000;
+const url = 'http://localhost:'.concat(port.toString());
+
+
+let imageId = 0;
+describe('Image API Routes', function () {
+
+  const passReqBody = {
+    name: "Derek",
+    userEmail: "derek123",
+    imagePath: "/datetime/imagePathInAWSS3Bucket",
+    tags: [
+      {
+        name: "A tag",
+        value: "A value"
+      },
+      {
+        name: "2nd tag",
+        value: "2nd value"
+      }
+    ]
+  }
+
+  let imageId = 0;
+  describe('POST /image', function () {
+    it('creates a successful entry in the image table', function (done: Mocha.Done) {
+      request(app)
+        .post('/image')
+        .send(passReqBody)
+        .expect(200)
+        .then(response => {
+          imageId = response.body.image.id;
+          assert.isObject(response);
+          done();
+        })
+    });
+  });
+
+  const failReqBody = { ...passReqBody, randomField: "abc" }
+
+  describe('POST /image', function () {
+    it('invalid request body', function (done: Mocha.Done) {
+      request(app)
+        .post('/image')
+        .send(failReqBody)
+        .expect(400)
+        .end(function (err, res) {
+          if (err) {
+            console.log(res);
+          }
+          done();
+        })
+    })
+  });
+});
